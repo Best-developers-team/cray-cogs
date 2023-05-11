7edc258be8528a9673494ed066944091a5238d9f
Mezbatheking <86008306+MEZBATHEKING@users.noreply.github.com>
2023-05-12 02:05:11 +0600

    Update __init__.py

--- giveaways/__init__.py
+++ giveaways/__init__.py
@@ -1,6 +1,6 @@
 import json
 from pathlib import Path
-
+from .giveaways import Giveaways
 from .gset import Gset
 
 with open(Path(__file__).parent / "info.json") as fp:
@@ -9,5 +9,5 @@ with open(Path(__file__).parent / "info.json") as fp:
 
 async def setup(bot):
    cog = Gset(bot)
-  await  bot.add_cog(cog)
-  await  bot.loop.create_task(cog.initialize())
+  await  bot.add_cog(Giveaways(bot))
+  bot.loop.create_task(cog.initialize())
